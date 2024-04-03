124ac6786098355a1aa55b4feaec92daa19d3ce2
ayush7111 <ayushrawat711@gmail.com>
2024-04-03 19:11:59 +0530

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const bookRoute = require("./routes/bookRoute");
 const Db = require("./config/Db");
-
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 Db();
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use("/api/books", bookRoute);
 
